7b238ed4e2b77c86cfa50f1c6a02d3d599ac4e56
ShardaJuhi21 <91040711+ShardaJuhi21@users.noreply.github.com>
2021-10-25 08:29:36 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -7,6 +7,12 @@ struct Stats compute_statistics(const float* numberset, int setlength)
     s.min = 0;
     s.max = 0;
 }
-
+check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
+{
+    flost max = maxThreshold ;
+    alerter_funcptr Array[] = alerters;
+    stats local = computedStats;
+    
+}
 int emailAlertCallCount = 0;
 int ledAlertCallCount = 0;
